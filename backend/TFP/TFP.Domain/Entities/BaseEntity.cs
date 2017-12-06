7aceafcbdc9b73d02c66ad527b3815bc219d5169
Rustam Guseynov <RustamGuseynov@coherentsolutions.com>
2017-12-06 18:09:25 +0300

    back-feature: BaseEntity

---
+++ backend/TFP/TFP.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace TFP.Domain.Entities
+{
+    public class BaseEntity
+    {
+        public DateTime CreatedDate { get; set; }
+
+        public Guid CreatedBy { get; set; }
+
+        public DateTime? ModifiedDate { get; set; }
+
+        public Guid? ModifiedBy { get; set; }
+
+        public bool Deleted { get; set; }
+    }
+}
